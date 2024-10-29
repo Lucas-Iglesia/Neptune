e04bc88ab7a3842f2cb8af1f6e185b0b3c75bfd6
Adrien Picot <picotadrien20@gmail.com>
2024-10-29 18:11:29 +0800

    (+) proto yolov7 + requirements.txt file

--- prototypes/yolo-models-testing/yolov7.py
+++ prototypes/yolo-models-testing/yolov7.py
@@ -1,30 +1,25 @@
 import torch
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).to(device)
+model = torch.hub.load('WongKinYiu/yolov7', 'yolov7', pretrained=True, force_reload=True).to(device)
 
-# freeze parameters for transfer learning
 for param in model.parameters():
     param.requires_grad = False
 
-# unfreeze layers for fine-tuning
 for module in model.modules():
     if hasattr(module, 'cv2'):
         for param in module.parameters():
             param.requires_grad = True
 
-# verify trainable parameters
 trainable_params = list(filter(lambda p: p.requires_grad, model.parameters()))
 if not trainable_params:
     raise ValueError("No parameters defined for fine-tuning; verify the layers configuration.")
 
 optimizer = torch.optim.Adam(trainable_params, lr=0.001)
 
-# Test model output with dummy input
 dummy_input = torch.randn(1, 3, 640, 640).to(device)
 output = model(dummy_input)
 
-# Display output shapes
 print("Output Shapes:")
 for i, out in enumerate(output):
     if isinstance(out, list):
